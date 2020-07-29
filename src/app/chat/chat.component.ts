5accf3c9d1a4d6a89c58fce6b96e2e010371fdd3
musiciancoder <andresguitarra1980@gmail.com>
2020-07-29 16:52:28 -0400

    204. Instalando librerías para el cliente WebSocket en Angular (sockjs y stompjs)

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Client} from '@stomp/stompjs';
+import * as SockJS from 'sockjs-client';
 
 @Component({
   selector: 'app-chat',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
+  private client: Client; //con esto nos conectamos, nos subscribimos a los eventos, etc.. trabajamos con el chat
 
   constructor() { }
 
   ngOnInit(): void {
+    this.client = new Client();
+    this.client.webSocketFactory = ()=>{
+      return new SockJS("http://localhost:8080/chat-websocket"); //URL de conexion que configuramos en Java Spring
+    }
+
+    //Con esto estamos escuchando cuando queramos conectarnos (pero aun no estamos conectados)
+    this.client.onConnect = (frame) => {
+      console.log('Conectados: ' + this.client.connected + ' : ' + frame);
+    }
+
+    //Con este metodo activated() sí que nos conectamos
+    this.client.activate();
+
   }
 
 }

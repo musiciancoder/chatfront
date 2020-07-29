313b03a6130e0e5bdf83bf6be1358057503d65e9
musiciancoder <andresguitarra1980@gmail.com>
2020-07-29 15:13:24 -0400

    203. Creando el proyecto frontend Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {ChatComponent} from './chat/chat.component';
 
-const routes: Routes = [];
+const routes: Routes = [   //<router-outlet> en app.component.html renderiza las rutas
+  {path: '',redirectTo:'chat', pathMatch: 'full'}, //para que por defecto redirija a chat
+  {path: 'chat', component: ChatComponent} //componente normal
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports:[RouterModule]
 })
-export class AppRoutingModule { }
+
+export  class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import {FormsModule} from '@angular/forms';
+import {AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
